1d7d3cab4622d59ebd385b5b6710e84634d28367
Vieirapa2 <117810778+Vieirapa2@users.noreply.github.com>
2022-11-09 08:29:19 +0000

    Add files via upload

---
+++ if2/if2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            bool isalmada = true;
+            bool istudent = true;
+            bool isalmada2 = false;
+            bool istudent2 = false;
+            //&& = and  || = or
+
+            if (isalmada && istudent2) 
+            {
+                Console.WriteLine("Es um estudante de alamda");
+            }else
+            { Console.WriteLine("nao es estudante ou nao es de alamda");
+            }Console.ReadLine(); 
+            */
+            bool isalmada = true;
+            bool istudent = true;
+            bool isalmada2 = false;
+            bool istudent2 = false;
+            //&& = and  || = or
+
+            if (isalmada || istudent2)
+            {
+                Console.WriteLine("es estudante ou es de alamda ou ambos");
+            }
+            else
+            {
+                Console.WriteLine("nao es um estudante de alamda");
+            }
+            Console.ReadLine();
+        }
+    }
+}
